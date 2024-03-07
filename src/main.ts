9c5d841e8c5fa9fb14af3bef37b3144866d7133c
auresolll <usool.203@gmail.com>
2024-03-08 00:13:56 +0700

    @

--- src/main.ts
+++ src/main.ts
@@ -6,8 +6,8 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   ConfigModule.forRoot();
   app.enableCors();
-  await app.listen(process.env.PORT || 3000);
+  await app.listen(3000);
 
-  console.log(app.getUrl());
+  console.log(await app.getUrl());
 }
 bootstrap();
